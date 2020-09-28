5410760c7e1ab7f5dd6abfb049579003f278714a
Jimmy Satterfield <fedora.game.dev@gmail.com>
2020-09-27 17:54:19 -0700

    Made MakeFrame more verbose

--- FrameOperators/MakeFrame.py
+++ FrameOperators/MakeFrame.py
@@ -1,28 +1,33 @@
 import bpy as blenderpy
+from bpy.types import Context, Object, Operator
+from typing import List
 from ..Debug import Debug
 from ..Static import StaticNames, StaticDirs
 
 
-class MakeFrame(blenderpy.types.Operator):
+class MakeFrame(Operator):
     bl_label = StaticNames.MakeFrameName
     bl_idname = StaticNames.MakeFrameID
 
-    def execute(self, context: blenderpy.types.Context):
-        objects = context.selected_objects
-        if len(objects) > 1:
-            Debug.LogError(
-                "Too many objects selected. Please ensure only 1 is selected.")
+    def execute(self, context: Context) -> any:
+        allSelectedObjects: List[Object] = context.selected_objects
+        lengthOfSelectedObjects: int = len(allSelectedObjects)
+        tooManyObjectsSelected: bool = lengthOfSelectedObjects > 1
+        noObjectSelected: bool = lengthOfSelectedObjects == 0
+
+        if tooManyObjectsSelected:
+            Debug.LogError("Please ensure only 1 object is selected.")
             return {"FINISHED"}
 
-        if len(objects) == 0:
+        if noObjectSelected:
             Debug.LogError("One object must be selected.")
             return {"FINISHED"}
 
-        obj = objects[0]
+        selectedObject: Object = allSelectedObjects[0]
 
-        self.MakeFrame(obj)
+        self.MakeFrame(selectedObject)
 
         return {"FINISHED"}
 
-    def MakeFrame(self, obj: blenderpy.types.Object) -> None:
+    def MakeFrame(self, obj: Object) -> None:
         Debug.Log(obj)
